006300e003a01b5711d36378d28344fd996dd2d9
sh1vrm <sanketh.mekala@edu.sait.ca>
2024-03-11 16:33:01 -0600

    header,css and footer added
    Added the components and changed some css

--- Group-Assignment/app/page.js
+++ Group-Assignment/app/page.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import RegistrationForm from "./components/RegistrationForm";
 import StudentList from "./components/StudentList";
 import Navbar from "./components/Navbar";
@@ -12,6 +13,18 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center p-8">
         <StudentList/>
         <RegistrationForm/>
+=======
+import Navbar from "./components/Navbar";
+import StudentForm from "./components/StudentForm";
+import Footer from "./components/Footer";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col h-screen justify-between">
+      <Navbar />
+      <main className="mb-auto">
+        <StudentForm />
+>>>>>>> Stashed changes
       </main>
       <Footer />
     </div>
