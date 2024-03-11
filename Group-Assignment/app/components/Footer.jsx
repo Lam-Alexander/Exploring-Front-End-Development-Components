7e44c8f0a6451c3371106d0eb2f018b7180862d5
Alex <lam.alexander@hotmail.com>
2024-03-11 17:37:27 -0600

    Adding Styling and Remove Feature

---
+++ Group-Assignment/app/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-4">
+      <div className="container mx-auto">
+        <p>&copy; 2024 New Generation High School. All rights reserved.</p>
+        <p>123 School Street, Calgary AB, Canada | Phone: 123-456-7890 | Email: <a href="#">info@newgenhighschool.com</a> </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
